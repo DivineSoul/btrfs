09904d337e4ac9802689dd69b10a5196e74afdab
Kyle Bader <kyle.bader@dreamhost.com>
2012-02-21 07:20:43 -0800

    BTRFS cookbook documentation
    Change-Id: I68e825e9501076be491ed64ac504e296aacef0f9
    Reviewed-on: https://git.newdream.net/4212
    Tested-by: Gerrit Admin <admin@gerrit.newdream.net>
    Reviewed-by: Brett Gailey <brett.gailey@dreamhost.com>

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,7 @@ maintainer_email	"kyle.bader@dreamhost.com"
 license				"Apache 2.0"
 description			"Installs tools for BTRFS filesystems"
 version				"0.0.1"
+
+%w{debian ubuntu}.each do |os|
+	supports os
+end
